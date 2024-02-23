a92664526482352dc86ace95834483b196707824
hamza-naeem=098 <hamzanaeem672@gmail.com>
2024-02-23 05:55:16 +0500

    java assignment 1

---
+++ app.js
@@ -0,0 +1,221 @@
+// // Chapter No 1 
+
+
+// //Q1
+// alert("Welcome to my website")
+// //Q2
+// alert("Error! Please enter a valid password.")
+// //Q3
+// alert("Welcome to JS Land...\n Happy Coding!")
+// //Q4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!"  )
+// //Q5
+// alert("Hello... I can run JS through my web browser's console")
+// console.log ("Hello... I can run JS through my web browser's console")
+// //Q6
+// //Q7
+//  alert("Hello world")
+
+
+
+
+
+// // Chapter No 2
+
+
+
+// //Q1
+// var Username = "hamza";
+
+// console.log(Username)
+
+// //Q2
+// var myname = "hamza"
+
+// var myname1 = "naeem"
+
+// var finalname = myname + " " +  myname1
+
+// console.log(finalname)
+
+// //Q3
+// var message = "Hello World"
+
+// console.log(message)
+
+// alert("Hello world")
+
+// //Q4
+
+// var name2 = "Jhone Doe"
+
+// console.log(name2)
+
+// alert("Jhone Doe")
+
+// var Age = "15 years old"
+
+// console.log(Age)
+
+// alert("15 years old")
+
+// var Course = "Certified Mobile Application Development"
+
+// console.log(Course)
+
+// alert("Certified Mobile Application Development")
+
+// //Q5
+
+// var Food = " PIZZA \n PIZZ \n PIZ \n PI \n P "
+
+// console.log(Food)
+
+// alert(" PIZZA \n PIZZ \n PIZ \n PI \n P ")
+
+// //Q6
+
+// var email = "hamzanaeem@example.com."
+
+// console.log(email)
+
+// alert(" My email address is hamzanaeem@gmail.com ")
+
+// //Q7
+
+// var book = "I am trying to learn from the Book A smarter \n way to learn JavaScript"
+
+// console.log(book)
+
+// alert("I am trying to learn from the Book A smarter \n way to learn JavaScript")
+
+// //Q8
+
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// //Q9
+
+// var design = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// console.log(design)
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
+
+// Chapter No 3
+
+
+
+// //Q1
+// var Age = "I am 15 years old"
+
+// console.log(Age)
+
+// alert("I am 15 years old")
+
+// //Q2 
+
+// var track = "You have visited this site"
+
+// var N = "14 time"
+
+// var trackfinal = track + " " + N
+
+// console.log(trackfinal)
+
+// alert("You have visited this site 14 time ")
+
+// //Q3
+
+// var birthyear = (" My birth year is 1990 </br> Data type of my declared variable is number")
+
+// console.log(birthyear)
+
+// document.write("My birth year is 1990 </br> Data type of my declared variable is number")
+
+// //Q4
+
+// var store = ("John Doe ordered 5 T-shirt(s) on XYZ Clothing store.")
+
+// console.log(store)
+
+// document.write(" <b>John Doe</b> ordered <b>5 T-shirt</b> (s) on XYZ Clothing store.")
+
+
+                          // Assignment
+
+//Chapter 1 (Alerts)
+
+
+// alert("Hamza");
+
+// alert("Naeem");
+
+// alert("hamzanaeem@gmail.com");
+
+// alert(123456789);
+
+// alert("You're learning JavaScript!");
+
+// alert("hello I am the student of jawan pakistan")
+
+
+//Chapter 2 (Variables for string)
+
+// Q1
+// var Name = 10
+// console.log(Name)
+
+// //Q2
+// var institude = "Jawn Pakistan"
+// console.log(institude)
+
+// //Q3
+// var teamName = ("Awesome Team")
+
+// console.log(teamName)
+
+// alert("teamName")
+
+// //Q4
+// var bestMan = ("Charlie")
+
+// bestMan = "hamza"
+
+// console.log(bestMan)
+
+
+//Chapter 3 (Variables for numbers)
+
+// //Q1
+// var caseQty = 2
+// console.log(caseQty)
+
+
+// //Q2
+// var caseQty1 = 144;
+// console.log(caseQty1)
+
+// //Q3
+// var num = 9;
+// console.log(num)
+
+// //Q4
+// var num1 = 5 
+// var num2 = 2
+// console.log(num1 + num2)
+
+// //Q5
+// var merchTotal = 100
+// var shippingCharge = 10
+
+// var orderTotal = merchTotal + shippingCharge
+// console.log(orderTotal)   
+
+
+// //Q6
+// var num3 = 4
+// var num4 = num3 + 3
+// console.log(num4)
